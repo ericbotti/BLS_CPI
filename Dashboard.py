a8040357cc73426bd9c15e2b671f319d01a0219b
Eric Bottinelli <ericbottinelli@gmail.com>
2024-08-25 00:47:21 +0200

    New data

--- Dashboard.py
+++ Dashboard.py
@@ -5,8 +5,11 @@ import numpy as np
 st.set_page_config(layout="wide")
 
 def load_data():
-    df = pd.read_csv('CPI_Data/cleaned_CPI_data.csv')
-    return df
+    NSA_MoM_df = pd.read_csv('CPI_Data/NSA_MoM_CPI_data.csv')
+    NSA_YoY_df = pd.read_csv('CPI_Data/NSA_YoY_CPI_data.csv')
+    SA_MoM_df = pd.read_csv('CPI_Data/SA_MoM_CPI_data.csv')
+    SA_YoY_df = pd.read_csv('CPI_Data/SA_YoY_CPI_data.csv')
+    return NSA_MoM_df, NSA_YoY_df, SA_MoM_df, SA_YoY_df
 
 def local_css(file_name):
     with open(file_name) as f:
@@ -28,28 +31,37 @@ def apply_styles(row):
         styles[2] = 'background-color: lightblue; color: black'
     return styles
 
-# Main app
-def main():
-    df = load_data() 
-
-    local_css("styles.css")
-
-    st.title('Structured Data Display')
-    st.write("Here is the structured data with heatmap styling:")
-
+def display_dataframe(df):
     df.replace(np.nan, '', inplace=True)
-
-    max_abs_value = df.iloc[0, 4:].abs().max().max()
-    
-    # Apply a color gradient with calculated vmin and vmax
+    max_abs_value = df.iloc[0, 4:].abs().max().max()  # Adjust to select from the right columns
     styled_df = df.style.format(formatter="{:.2%}", subset=df.columns[4:]).background_gradient(
         cmap='RdYlGn', subset=df.columns[4:], vmin=-max_abs_value, vmax=max_abs_value
     ).apply(apply_styles, axis=1, subset=['Category', 'Sub Category 1', 'Sub Category 2']).hide(axis="index")
-    
     html = styled_df.to_html()
-
-    # Display the styled DataFrame
     st.markdown(html, unsafe_allow_html=True)
+
+
+# Main app
+def main():
+    NSA_MoM_df, NSA_YoY_df, SA_MoM_df, SA_YoY_df = load_data()
+
+    local_css("styles.css")
+
+    st.title('BLS CPI Data Analysis')
+    st.write("Analysis of the Consumer Price Index (CPI) data from the Bureau of Labor Statistics (BLS).")
+
+    SA = st.radio("Seasonality Adjustment:", ('Yes', 'No'))
+
+    MoM_YoY = st.radio("Percentage change:", ('Month over Month', 'Year over Year'))   
+    
+    if SA == 'Yes' and MoM_YoY == 'Month over Month':
+        display_dataframe(SA_MoM_df)
+    elif SA == 'Yes' and MoM_YoY == 'Year over Year':
+        display_dataframe(SA_YoY_df)
+    elif SA == 'No' and MoM_YoY == 'Month over Month':
+        display_dataframe(NSA_MoM_df)
+    elif SA == 'No' and MoM_YoY == 'Year over Year':
+        display_dataframe(NSA_YoY_df)
 
 
 if __name__ == "__main__":
